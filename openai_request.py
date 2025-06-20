ec43d5ef5095d4543ffec678e336c4422c29c7b0
Devanshi Gupta <130989579+guptadevanshi@users.noreply.github.com>
2025-06-21 00:39:36 +0530

    Update openai_request.py

--- openai_request.py
+++ openai_request.py
@@ -3,19 +3,6 @@ import user_config
 
 client = OpenAI(api_key=user_config.openai_key)
 
-# def send_request(query):
-#     completion = client.chat.completions.create(
-#         model="gpt-4o-mini",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": query
-#             }
-#         ]
-#     )
-
-#     return completion.choices[0].message.content
-
 def send_request(query):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
